555a7ec55de56f0ef32ef85e72eb29bcc0a44afb
DONGROCK <ehdfkr8022@naver.com>
2023-10-14 18:16:24 +0900

    commit

---
+++ 00_java/com.demo/src/main/java/Demo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("숫자를 입력하시오 : ");
+        int num = sc.nextInt();     //숫자를 입력받아
+        if (num > 100){
+            System.out.println("100보다 큰 수 입니다.");
+        } else {
+            System.out.println("100보다 작은 수 입니다.");
+        }
+    }
+}
